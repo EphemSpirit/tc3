67b3d5a893a3432c05570f34eaf6ab6909e22c27
Drew Hund <languageinvestigator@gmail.com>
2021-02-10 18:08:04 -0500

    journals up and running

---
+++ app/controllers/journals_controller.rb
@@ -0,0 +1,61 @@
+class JournalsController < ApplicationController
+  before_action :find_journal, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @journals = current_user.journals
+  end
+
+  def new
+    @journal = current_user.journals.build
+  end
+
+  def create
+    @journal = current_user.journals.build(journal_params)
+
+    if @journal.save
+      redirect_to @journal
+      flash[:notice] = "Add tags to make this entry searchable!"
+    else
+      render 'new'
+      flash[:notice] = "Something went wrong"
+    end
+  end
+
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @journal.update(journal_params)
+    redirect_to root_path
+  end
+
+
+  def destroy
+    @journal.destroy
+    flash[:notice] = "Entry successfully deleted"
+    redirect_to my_entries_path
+  end
+
+  private
+
+    def find_journal
+      @journal = Journal.find(params[:id])
+    end
+
+    def journal_params
+      params.require(:journal).permit(:title, :body)
+    end
+
+    # def permitted_params
+    #   params.require(:journal).permit(:title, :body, tags: [])
+    # end
+    #
+    # def tags
+    #   permitted_params.slice(:tags)
+    # end
+
+end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,15 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "semantic-ui-sass"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+$(document).on('turbolinks:load', function() {
+  $('.ui.dropdown').dropdown();
+  $('.message .close').on('click', function() {
+    $(this).closest('.message').transition('fade');
+  });
+})
